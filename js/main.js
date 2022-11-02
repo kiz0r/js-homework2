f7932160de00cb2685727eb1eda5a382ba856cf5
Kirill Khomenko <kkhomenko10@gmail.com>
2022-11-02 23:15:00 +0100

    similar tasks but with cycle while

--- js/main.js
+++ js/main.js
@@ -62,3 +62,30 @@ for (let startPoint = 1; startPoint <= 100; startPoint++) {
   result += startPoint;
 }
 console.log("Сумма чисел від 1 до 100 =", result);
+
+/*Реализация заданий с циклами через while*/
+
+//  console.log("ВІД 25 ДО 0");
+// let num = 25;
+// while(num >= 0) {
+//   console.log(num);
+//   num--;
+// }
+
+// // Виведення від 10 до 50 (кожен раз збільшити попереднє значення на 5)
+// console.log("ВІД 10 ДО 50 (КОЖЕН РАЗ +5)");
+// const numberCounter = 5;
+// let startNumber = 10;
+// while(startNumber <= 50) {
+//   console.log(startNumber);
+//   startNumber += numberCounter;
+// }
+
+// // Сумма чисел від 1 до 100
+// let result = 0;
+// let startPoint = 1;
+// while(startPoint <= 100) {
+//   result += startPoint;
+//   startPoint++;
+// }
+// console.log("Сумма чисел від 1 до 100 =", result);
